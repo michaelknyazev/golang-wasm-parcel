8e0cdb816fd205aaf93c6da3b22c8e35948bc0ed
Michael Knyazev <mikebradius@gmail.com>
2022-01-25 23:18:00 +0300

    First Steps

--- src/main.go
+++ src/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth  = 320
+	screenHeight = 320
+)
+
+type Game struct {
+	count int
+}
+
+func (g *Game) Update() error {
+	g.count++
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.NRGBA{0xf0, 0xf0, 0xf0, 0xf0})
+	fmt.Println(g.count)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
