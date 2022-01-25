a7085f2e8cfbf84265c5be1be337928fdf1d625d
Michael Knyazev <mikebradius@gmail.com>
2022-01-25 23:45:24 +0300

    Character Move.

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
@@ -16,6 +17,8 @@ const (
 
 	frameWidth  = 65
 	frameHeight = 65
+
+	speed = 10
 )
 
 var (
@@ -25,19 +28,66 @@ var (
 type Game struct {
 	stanceY int
 	stanceX int
+
+	posY int
+	posX int
 }
 
 func (g *Game) Update() error {
+
+	_keyMoveUp := inpututil.IsKeyJustPressed(ebiten.KeyW)
+	_keyMoveDown := inpututil.IsKeyJustPressed(ebiten.KeyS)
+	_keyMoveLeft := inpututil.IsKeyJustPressed(ebiten.KeyA)
+	_keyMoveRight := inpututil.IsKeyJustPressed(ebiten.KeyD)
+
+	// Move Control
+	if _keyMoveUp {
+		g.posY -= speed
+
+		if g.posY <= 0 {
+			g.posY = 0
+		}
+	}
+
+	if _keyMoveDown {
+		g.posY += speed
+
+		if g.posY >= screenHeight {
+			g.posY = screenHeight
+		}
+	}
+
+	if _keyMoveLeft {
+		g.posX -= speed
+
+		if g.posX <= 0 {
+			g.posX = 0
+		}
+	}
+
+	if _keyMoveRight {
+		g.posX += speed
+
+		if g.posX >= screenWidth {
+			g.posX = screenWidth
+		}
+	}
+	// End Move Control
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.NRGBA{0xf0, 0xf0, 0xf0, 0xf0})
 
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(float64(g.posX), float64(g.posY))
+
 	sx, sy := g.stanceX*frameWidth, g.stanceY*frameHeight
 	frame := characterImage.SubImage(image.Rect(sx, sy, sx+frameHeight, sy+frameWidth)).(*ebiten.Image)
 
-	screen.DrawImage(frame, nil)
+	screen.DrawImage(frame, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -52,7 +102,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := ebiten.RunGame(&Game{0, 0}); err != nil {
+	if err := ebiten.RunGame(&Game{0, 0, 0, 0}); err != nil {
 		log.Fatal(err)
 	}
 }
